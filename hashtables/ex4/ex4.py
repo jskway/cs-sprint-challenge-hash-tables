60f55439f90598070826be6e52bc3ad059bf9fb4
jackskim <jackskim29@gmail.com>
2020-07-17 20:54:04 -0700

    Add solution for ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -2,7 +2,42 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # Set up hash table
+    # key: number
+    # value: boolean (list contains its negation)
+    has_opp = {}
+
+    # Set up result list
+    result = []
+
+    # Iterate through the nums:
+    for num in a:
+        # If positive
+        if num > 0:
+            # If positive num not in hash table
+            if num not in has_opp:
+                # Initialize entry with False
+                has_opp[num] = False
+            # If negative version is hash
+            if -num in has_opp:
+                has_opp[-num] = True
+                has_opp[num] = True
+        # If negative
+        else:
+            # If negative num not in hash table
+            if num not in has_opp:
+                # Initialize entry with false
+                has_opp[num] = False
+            # If positive version is in hash table
+            if abs(num) in has_opp:
+                # Set value to true
+                has_opp[abs(num)] = True
+                has_opp[num] = True
+
+    # Iterate through the entries
+    for num, boolean in has_opp.items():
+        if boolean is True and num > 0:
+            result.append(num)
 
     return result
 
